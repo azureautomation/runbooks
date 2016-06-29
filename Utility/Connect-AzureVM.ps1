cc50db4dd3070d65dbfb759716c1a7798bde67dc
rohitminni <romin@microsoft.com>
2016-06-29 19:35:32 +0530

    Updated to incorporate proper naming conventions

--- Utility/Connect-AzureVM.ps1
+++ Utility/Connect-AzureVM.ps1
@@ -1,6 +1,6 @@
 <#PSScriptInfo
 
-.VERSION 1.0
+.VERSION 2.0
 
 .GUID 803969d1-8f13-482b-8345-6b5b503dff25
 
@@ -57,7 +57,7 @@
 
 .NOTES
     AUTHOR: Rohit Minni
-    LASTEDIT: May 25, 2016 
+    LASTEDIT: June 29, 2016 
 #>
    
     Param
@@ -112,56 +112,56 @@
           ) 
  
 # define a temporary file in the users TEMP directory
-$file = $env:TEMP + "\ConfigureWinRM_HTTPS.ps1"
+$File = $env:TEMP + "\ConfigureWinRM_HTTPS.ps1"
 
-$string = "param(`$DNSName)" + "`r`n" + "Enable-PSRemoting -Force" + "`r`n" + "New-NetFirewallRule -Name 'WinRM HTTPS' -DisplayName 'WinRM HTTPS' -Enabled True -Profile 'Any' -Action 'Allow' -Direction 'Inbound' -LocalPort 5986 -Protocol 'TCP'" + "`r`n" + "`$thumbprint = (New-SelfSignedCertificate -DnsName `$DNSName -CertStoreLocation Cert:\LocalMachine\My).Thumbprint" + "`r`n" + "`$cmd = `"winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=`"`"`$DNSName`"`"; CertificateThumbprint=`"`"`$thumbprint`"`"}`"" + "`r`n" + "cmd.exe /C `$cmd"
+$String = "param(`$DNSName)" + "`r`n" + "Enable-PSRemoting -Force" + "`r`n" + "New-NetFirewallRule -Name 'WinRM HTTPS' -DisplayName 'WinRM HTTPS' -Enabled True -Profile 'Any' -Action 'Allow' -Direction 'Inbound' -LocalPort 5986 -Protocol 'TCP'" + "`r`n" + "`$thumbprint = (New-SelfSignedCertificate -DnsName `$DNSName -CertStoreLocation Cert:\LocalMachine\My).Thumbprint" + "`r`n" + "`$cmd = `"winrm create winrm/config/Listener?Address=*+Transport=HTTPS @{Hostname=`"`"`$DNSName`"`"; CertificateThumbprint=`"`"`$thumbprint`"`"}`"" + "`r`n" + "cmd.exe /C `$cmd"
 	
-$string | Out-File -FilePath $file -force
+$String | Out-File -FilePath $File -force
  
   
 # Get the VM we need to configure
-$vm = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
+$VM = Get-AzureRmVM -ResourceGroupName $ResourceGroupName -Name $VMName
  
 # Get storage account name
-$StorageAccountName = $vm.StorageProfile.OsDisk.Vhd.Uri.Split("//")[2].Split('.')[0]
+$StorageAccountName = $VM.StorageProfile.OsDisk.Vhd.Uri.Split("//")[2].Split('.')[0]
 # get storage account key
 $StorageKey = Get-AzureRmStorageAccountKey -ResourceGroupName $ResourceGroupName -Name $StorageAccountName
 try
 {
-    $key = $StorageKey.value[0]
+    $Key = $StorageKey.value[0]
 }
 catch
 {
 }
 finally
 {
-    if($key -eq $null)
+    if($Key -eq $null)
     {
-        $key = $StorageKey.Key1
+        $Key = $StorageKey.Key1
     }
 }
 # create storage context
-$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $key
+$StorageContext = New-AzureStorageContext -StorageAccountName $StorageAccountName -StorageAccountKey $Key
   
 # create a container called scripts
 $CreateContainer = New-AzureStorageContainer -Name "scripts" -Context $StorageContext -ErrorAction SilentlyContinue
   
 #upload the file
-$BlobContent = Set-AzureStorageBlobContent -Container "scripts" -File $file -Blob "ConfigureWinRM_HTTPS.ps1" -Context $StorageContext -force
+$BlobContent = Set-AzureStorageBlobContent -Container "scripts" -File $File -Blob "ConfigureWinRM_HTTPS.ps1" -Context $StorageContext -force
  
 # Create custom script extension from uploaded file
-$extension = Set-AzureRmVMCustomScriptExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Name "EnableWinRM_HTTPS" -Location $vm.Location -StorageAccountName $StorageAccountName -StorageAccountKey $key -FileName "ConfigureWinRM_HTTPS.ps1" -ContainerName "scripts" -RunFile "ConfigureWinRM_HTTPS.ps1" -Argument $DNSName
+$Extension = Set-AzureRmVMCustomScriptExtension -ResourceGroupName $ResourceGroupName -VMName $VMName -Name "EnableWinRM_HTTPS" -Location $VM.Location -StorageAccountName $StorageAccountName -StorageAccountKey $Key -FileName "ConfigureWinRM_HTTPS.ps1" -ContainerName "scripts" -RunFile "ConfigureWinRM_HTTPS.ps1" -Argument $DNSName
 
 # Get the name of the first NIC in the VM
-$nicName = Get-AzureRmResource -ResourceId $vm.NetworkInterfaceIDs[0]
-$nic = Get-AzureRmNetworkInterface -ResourceGroupName $ResourceGroupName -Name $nicName.ResourceName
+$NicName = Get-AzureRmResource -ResourceId $VM.NetworkInterfaceIDs[0]
+$Nic = Get-AzureRmNetworkInterface -ResourceGroupName $ResourceGroupName -Name $NicName.ResourceName
  
 # Get the network security group attached to the NIC
-$nsgRes = Get-AzureRmResource -ResourceId $nic.NetworkSecurityGroup.Id
-$nsg = Get-AzureRmNetworkSecurityGroup  -ResourceGroupName $ResourceGroupName  -Name $nsgRes.Name 
+$NsgRes = Get-AzureRmResource -ResourceId $nic.NetworkSecurityGroup.Id
+$Nsg = Get-AzureRmNetworkSecurityGroup  -ResourceGroupName $ResourceGroupName  -Name $NsgRes.Name 
   
 # Add the new NSG rule, and update the NSG
-$InboundRule = $nsg | Add-AzureRmNetworkSecurityRuleConfig -Name "WinRM_HTTPS" -Priority 1100 -Protocol TCP -Access Allow -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5986 -Direction Inbound -ErrorAction SilentlyContinue | Set-AzureRmNetworkSecurityGroup -ErrorAction SilentlyContinue
+$InboundRule = $Nsg | Add-AzureRmNetworkSecurityRuleConfig -Name "WinRM_HTTPS" -Priority 1100 -Protocol TCP -Access Allow -SourceAddressPrefix $SourceAddressPrefix -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 5986 -Direction Inbound -ErrorAction SilentlyContinue | Set-AzureRmNetworkSecurityGroup -ErrorAction SilentlyContinue
 
 }
 
@@ -173,33 +173,33 @@ $InboundRule = $nsg | Add-AzureRmNetworkSecurityRuleConfig -Name "WinRM_HTTPS" -
   
         $NICs = Get-AzureRmNetworkInterface | Where-Object{$_.VirtualMachine.Id -eq $VM.Id}
         $IPConfigArray = New-Object System.Collections.ArrayList
-        foreach($nic in $NICs)
+        foreach($Nic in $NICs)
         {
-            if($nic.IpConfigurations.LoadBalancerBackendAddressPools)
+            if($Nic.IpConfigurations.LoadBalancerBackendAddressPools)
             {
-                $arr = $nic.IpConfigurations.LoadBalancerBackendAddressPools.id.Split('/')
-                $LoadBalancerNameIndex = $arr.IndexOf("loadBalancers") + 1                    
-                $loadBalancer = Get-AzureRmLoadBalancer | Where-Object{$_.Name -eq $arr[$LoadBalancerNameIndex]}
-                $PublicIpId = $loadBalancer.FrontendIPConfigurations.PublicIpAddress.Id
+                $Arr = $Nic.IpConfigurations.LoadBalancerBackendAddressPools.id.Split('/')
+                $LoadBalancerNameIndex = $Arr.IndexOf("loadBalancers") + 1                    
+                $LoadBalancer = Get-AzureRmLoadBalancer | Where-Object{$_.Name -eq $Arr[$LoadBalancerNameIndex]}
+                $PublicIpId = $LoadBalancer.FrontendIPConfigurations.PublicIpAddress.Id
             }
 
-            $publicips = New-Object System.Collections.ArrayList
+            $PublicIps = New-Object System.Collections.ArrayList
 
-            if($nic.IpConfigurations.PublicIpAddress.Id)
+            if($Nic.IpConfigurations.PublicIpAddress.Id)
             {
-                $publicips.Add($nic.IpConfigurations.PublicIpAddress.Id) | Out-Null
+                $PublicIps.Add($Nic.IpConfigurations.PublicIpAddress.Id) | Out-Null
             }
 
             if($PublicIpId)
             {
-                $publicips.Add($PublicIpId) | Out-Null
+                $PublicIps.Add($PublicIpId) | Out-Null
             }
 
-            foreach($publicip in $publicips)
+            foreach($PublicIp in $PublicIps)
             {
-                $name = $publicip.split('/')[$publicip.Split('/').Count - 1]
-                $ResourceGroup = $publicip.Split('/')[$publicip.Split('/').Indexof("resourceGroups")+1]
-                $PublicIPAddress = Get-AzureRmPublicIpAddress -Name $name -ResourceGroupName $ResourceGroup | Select-Object -Property Name,ResourceGroupName,Location,PublicIpAllocationMethod,IpAddress
+                $Name = $PublicIp.split('/')[$PublicIp.Split('/').Count - 1]
+                $ResourceGroup = $PublicIp.Split('/')[$PublicIp.Split('/').Indexof("resourceGroups")+1]
+                $PublicIPAddress = Get-AzureRmPublicIpAddress -Name $Name -ResourceGroupName $ResourceGroup | Select-Object -Property Name,ResourceGroupName,Location,PublicIpAllocationMethod,IpAddress
                 $IPConfigArray.Add($PublicIPAddress) | Out-Null
             }
         }
