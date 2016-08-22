6ddf3fd71d50d7371ff85a15c4b12a0137166126
Jenny Hunter <jehunte@microsoft.com>
2016-08-22 09:57:31 -0700

    Added DSC Configuration file for Hybrid Worker

---
+++ Utility/HybridWorkerConfiguration.ps1
@@ -0,0 +1,23 @@
+﻿Configuration HybridWorkerConfiguration {
+    Import-DscResource -ModuleName HybridRunbookWorker
+
+    param(
+
+        [Parameter(Mandatory=$true)]
+        [String] $Endpoint,
+
+        [Parameter(Mandatory=$true)]
+        [String] $Token,
+
+        [Parameter(Mandatory=$false)]
+        [String] $GroupName = "MyHybridWorker"
+
+    )
+
+    HybridRunbookWorker Onboard {
+        Ensure = 'Present'
+        Endpoint = $Endpoint
+        Token = $Token
+        GroupName = $GroupName
+    }
+}
