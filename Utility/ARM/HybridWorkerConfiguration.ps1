5837c67be12951d4e5f76aeaa6f90c3105d54616
Jenny Hunter <jehunte@microsoft.com>
2016-10-11 16:59:51 -0700

    Update Hybrid Worker DSC Config to check for MMA
    - Switched to using DSC to install and check for the monitoring agent

--- Utility/ARM/HybridWorkerConfiguration.ps1
+++ Utility/ARM/HybridWorkerConfiguration.ps1
@@ -11,6 +11,12 @@
     )
 
     Import-DscResource -ModuleName HybridRunbookWorker
+    Import-DscResource -ModuleName xPSDesiredStateConfiguration -ModuleVersion 4.0.0.0
+
+    $OIPackageLocalPath = "C:\MMASetup-AMD64.exe"
+    $WorkspaceID = Get-AutomationVariable -Name "WorkspaceId" 
+    $WorkspaceKey = Get-AutomationVariable -Name "WorkspaceKey"
+
 
 
     Node $AllNodes.NodeName {
@@ -19,6 +25,27 @@
             Endpoint  = $AutomationEndpoint
             Token     = Get-AutomationPSCredential -Name "TokenCredential"
             GroupName = $HybridGroupName
+        }
+
+         xRemoteFile OIPackage {
+            Uri = "https://opsinsight.blob.core.windows.net/publicfiles/MMASetup-AMD64.exe"
+            DestinationPath = $OIPackageLocalPath
+        }
+
+        Service OIService
+        {
+            Name = "HealthService"
+            State = "Running"
+            DependsOn = "[Package]OI"
+        }
+
+        Package OI {
+            Ensure = "Present"
+            Path  = $OIPackageLocalPath
+            Name = "Microsoft Monitoring Agent"
+            ProductId = "E854571C-3C01-4128-99B8-52512F44E5E9"
+            Arguments = '/C:"setup.exe /qn ADD_OPINSIGHTS_WORKSPACE=1 OPINSIGHTS_WORKSPACE_ID=' + $WorkspaceID + ' OPINSIGHTS_WORKSPACE_KEY=' + $WorkspaceKey + ' AcceptEndUserLicenseAgreement=1"'
+            DependsOn = "[xRemoteFile]OIPackage"
         }
     }
 }
