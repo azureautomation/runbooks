80cced11053107b3e206e4de5eaa8d21c4edf1e3
Jenny Hunter <jehunte@microsoft.com>
2016-10-11 17:00:13 -0700

    Added a DSC Resource to Remove Hybrid Workers

---
+++ Utility/ARM/RemoveHybridWorker.ps1
@@ -0,0 +1,23 @@
+﻿Configuration RemoveHybridWorker {
+
+    param(
+
+        [Parameter(Mandatory=$true)]
+        [System.String] $AutomationEndpoint,
+
+        [Parameter(Mandatory=$true)]
+        [System.String] $HybridGroupName = "MyHybridWorkerGroup"
+
+    )
+
+    Import-DscResource -ModuleName HybridRunbookWorker
+
+    Node $AllNodes.NodeName {
+        HybridRunbookWorker Onboard {
+            Ensure    = 'Absent'
+            Endpoint  = $AutomationEndpoint
+            Token     = Get-AutomationPSCredential -Name "TokenCredential"
+            GroupName = $HybridGroupName
+        }
+    }
+}
