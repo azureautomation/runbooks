efeaaacb2b107479120cecd9e93a84b3be9ea00d
Eamon O'Reilly <eamono@microsoft.com>
2017-11-14 14:07:38 -0800

    Sample watcher action runbook

---
+++ Utility/ARM/Process-Watcher.ps1
@@ -0,0 +1,24 @@
+<#
+.SYNOPSIS 
+    This sample automation runbook is designed to be used in a watcher task that takes action
+    on data passed in from a watcher runbook.
+
+.DESCRIPTION
+    This sample automation runbook is designed to be used in a watcher task that takes action
+    on data passed in from a watcher runbook. It is required to have a parameter called $EVENTDATA in
+    watcher action runbooks to receive information from the watcher runbook.
+
+.PARAMETER EVENTDATA
+    Optional. Contains the information passed in from the watcher runbook.
+
+.NOTES
+    AUTHOR: Automation Team
+    LASTEDIT: Nov 12th, 2017
+#>
+
+param(
+    $EVENTDATA
+)
+
+Write-Output("Message is " + $EVENTDATA.EventProperties.Message)
+Write-Output("Passed in data is " + ($EVENTDATA.EventProperties.Data | ConvertFrom-Json))
