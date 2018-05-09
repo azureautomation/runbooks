8c106f3bbdbd0605c8b636c673fdc1a14d968431
Jenny Hunter <jehunte@microsoft.com>
2018-05-09 12:26:52 -0700

    Updated for new module
    - Updated the change report script to receive queries from the AzureRm.OperationalInsights module

--- Utility/ARM/New-ChangeReportEmail.ps1
+++ Utility/ARM/New-ChangeReportEmail.ps1
@@ -1,16 +1,16 @@
 ﻿<#PSScriptInfo
 
-.VERSION 1.0
+.VERSION 1.1
 
 .GUID 02c1b8eb-28ff-4b7f-9935-3c9285370cd7
 
 .AUTHOR Azure Automation Team
 
-.COMPANYNAME 'Microsoft Corporation'
+.COMPANYNAME Microsoft Corporation
 
-.COPYRIGHT 'Microsoft Corporation. All rights reserved.'
+.COPYRIGHT Microsoft Corporation. All rights reserved.
 
-.TAGS 'Azure', 'Azure Automation', 'Change Tracking', 'Email'
+.TAGS Azure, Azure Automation, Change Tracking, Email, Report
 
 .LICENSEURI https://github.com/azureautomation/runbooks/blob/master/LICENSE
 
@@ -18,15 +18,20 @@
 
 .ICONURI 
 
-.EXTERNALMODULEDEPENDENCIES  @(@{ModuleName = 'AzureRM.Profile'; ModuleVersion = '4.5.0'; }, 
-            @{ModuleName = 'AzureRM.OperationalInsights'; ModuleVersion = '4.5.0'; },
-            @{ModuleName = 'LogAnalyticsQuery';})
+.EXTERNALMODULEDEPENDENCIES
 
 .REQUIREDSCRIPTS 
 
 .EXTERNALSCRIPTDEPENDENCIES 
 
 .RELEASENOTES 
+
+1.1 - 4/24/2018
+
+ -- EDITED BY Jenny Hunter
+
+ -- modified to use the new Log Analytyics module in the AzureRM.OperationalInsights module
+
 
 1.0 - 4/5/2018
 
@@ -36,6 +41,10 @@
 
 
 #>
+
+#Requires -Module @{ModuleName = 'AzureRM.Profile'; ModuleVersion = '4.6.0';}
+
+#Requires -Module @{ModuleName = 'AzureRM.OperationalInsights'; ModuleVersion = '4.3.2';}
 
 <#
 
@@ -47,38 +56,30 @@
 .DESCRIPTION
 
     This runbook generates a report of changes from the past week and emails it. Note: This script requires two modules:
-    the AzureRM.OperationalInsights module and the new Log Analytics custom module. In order for the script to work, both
-    modules must first be imported into the Automation account. The Log Analytics module is not currently available from 
-    the PS Gallery and must be downloaded from https://dev.loganalytics.io/oms/documentation/4-Tools/LogAnalyticsQuery.psm1 .
+    the AzureRM.OperationalInsights module. In order for the script to work, both modules must first be imported into 
+    the Automation account.
     
     The major steps of the script are outlined below. 
 
     1) Define parsing function
     2) Connect to the Azure account
     3) Set the subscription context
-    4) Set query timespan to one week
-    5) Prepare email HTML
-    6) Define, run, and parse Log Analytics queries for the report
-    7) Send the email
+    4) Prepare email HTML
+    5) Define, run, and parse Log Analytics queries for the report
+    6) Send the email
 
 
-.PARAMETER OMSResourceGroupName
+.PARAMETER WorkspaceID
 
-    Mandatory. The name of the resource group to be referenced for the OMS workspace. If not specified,
-    
-    the AAResourceGroupName is useed.
+    Mandatory. The ID of the OMS Workspace to be referenced. 
 
 
 .PARAMETER SubscriptionID
 
-    Mandatory. A string containing the SubscriptionID to be used. 
+    Optional. A string containing the SubscriptionID of the Azure Automation account to be referenced for the dashboard 
 
-
-.PARAMETER WorkspaceName
-
-    Mandatory. The name of the OMS Workspace to be referenced. If not specified, a new OMS workspace 
-
-    is created using a unique identifier.
+    link. If not specified, the value will remain null, and no link to the Change Tracking dashboard will be created.
+ 
 
 
 .PARAMETER AAName
@@ -118,14 +119,14 @@
 
 .EXAMPLE
 
-    New-ChangeReportEmail -WorkspaceName "ContosoAA" -OMSResourceGroupName "ContosoResources" -SubscriptionId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" -CredentialName "AAEmailCred" -EmailTo "jenny@contoso.com" -EmailFrom "it@contoso.com"
+    New-ChangeReportEmail -WorkspaceId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx" -SubscriptionId "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx" -CredentialName "AAEmailCred" -EmailTo "jenny@contoso.com" -EmailFrom "it@contoso.com"
 
 
 .NOTES
 
     AUTHOR: Jenny Hunter, Azure Automation Team
 
-    LASTEDIT: April 4, 2018
+    LASTEDIT: April 24, 2018
 
     EDITBY: Jenny Hunter
 
@@ -134,14 +135,10 @@
 Param (
 # OMS Workspace
 [Parameter(Mandatory=$true)]
-[String] $WorkspaceName,
-
-[Parameter(Mandatory=$true)]
-[String] $OMSResourceGroupName,
-
+[String] $WorkspaceId,
 
 # Subscription
-[Parameter(Mandatory=$true)]
+[Parameter(Mandatory=$false)]
 [String] $SubscriptionID,
 
 # Automation Account
@@ -177,16 +174,9 @@ param (
 [String[]] $Results
 )
     $b = ""
-
-   if (($Results.Length -eq 2) -and ($Results[0] -notmatch "\d" )) {
-        $b += "<p>" + $Results[0] + " : " + $Results[1] + "</p>"
-    } else {
-        foreach ($value in $Results) {
-            $b += "<p>"
-            $b += $value -Replace " ", " : "
-            $b += "</p>"
-        }
-    }
+   for ($i = 0; $i -lt $Results.Length; $res++) {
+        $b += "<p>" + $Results[$i] + "</p>"
+   }
     
     Return $b
 }
@@ -194,26 +184,13 @@ param (
 # Connect to the current Azure account
 $Conn = Get-AutomationConnection -Name AzureRunAsConnection 
 $null = Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID -ApplicationID $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint 
-
-# Set the active subscription
-$null = Set-AzureRmContext -SubscriptionID $SubscriptionID
-
-# Check that the resource groups are valid
-$null = Get-AzureRmResourceGroup -Name $OMSResourceGroupName
-
-# Check that the OMS Workspace is valid
-$null = Get-AzureRmOperationalInsightsWorkspace -Name $WorkspaceName -ResourceGroupName $OMSResourceGroupName  -ErrorAction Stop
-
-# Timespan
-$now = Get-Date
-$week = $now.AddDays(-7)
-$isotimespan = $week.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ss.fffffffZ") + "/" + $now.ToUniversalTime().ToString( "yyyy-MM-ddTHH:mm:ss.fffffffZ")
+#Add-AzureRmAccount
 
 # Create the HTML
 $a = '<table align="center" style="border: 1px solid #cccccc; "border="0" cellpadding="0" cellspacing="0" width="600"> <tr> <td bgcolor="#26619C" align="center" style="padding: 20px 0 30px 0; color: #ffffff; font-family: Arial, sans-serif; font-size: 24px;"> <b>Azure Change Tracking </b> <p style="font-size: 20px;">Change Report for ' + $week.ToShortDateString() + " - " + $now.ToShortDateString() + '</p> </td> </tr> <tr> <td bgcolor="#ffffff">'
 
 # Add link to Chaneg Tracking in Azure if AA info was supplied and is accurate
-if ($AAName -and $AAResourceGroupName) {
+if ($AAName -and $AAResourceGroupName -and $SubscriptionID) {
     try {
         $null = Get-AzureRmAutomationAccount -ResourceGroupName $AAResourceGroupName -Name $AAName
         $link = "https://portal.azure.com/#@microsoft.onmicrosoft.com/resource/subscriptions/$SubscriptionID/resourceGroups/$AAResourceGroupName/providers/Microsoft.Automation/automationAccounts/$AAName/changeTrackingTrackChanges"
@@ -231,29 +208,15 @@ $a += '<table border="0" cellpadding="0" cellspacing="0" width="100%"> <tr> <td 
 
 # Define queries
 $ChangeTypesQuery = "ConfigurationChange | summarize count() by ConfigChangeType | sort by count_ desc"
-$TopComputersQuery = "ConfigurationChange | summarize ChangeCount= count() by Computer | order by ChangeCount desc | limit 5"
+$TopComputersQuery = "ConfigurationChange | summarize count() by Computer | order by ChangeCount desc | limit 5"
 $SoftwareAddedQuery = 'ConfigurationChange | where ConfigChangeType == "Software" and ChangeCategory == "Added" | summarize count() by SoftwareType'
 $ServicesStoppedQuery = 'ConfigurationChange | where ConfigChangeType == "WindowsServices" and SvcState == "Stopped" | summarize count() by SvcStartupType'
 
 # Collect results
-$ChangeTypesResults = (Invoke-LogAnalyticsQuery -WorkspaceName $WorkspaceName -ResourceGroup $OMSResourceGroupName -SubscriptionId $SubscriptionID -Query $ChangeTypesQuery -Timespan $isotimespan).Response.Content
-$TopComputersResults = (Invoke-LogAnalyticsQuery -WorkspaceName $WorkspaceName -ResourceGroup $OMSResourceGroupName -SubscriptionId $SubscriptionID -Query $TopComputersQuery -Timespan $isotimespan).Response.Content
-$SoftwareAddedResults = (Invoke-LogAnalyticsQuery -WorkspaceName $WorkspaceName -ResourceGroup $OMSResourceGroupName -SubscriptionId $SubscriptionID -Query $SoftwareAddedQuery -Timespan $isotimespan).Response.Content
-$ServicesStoppedResults = (Invoke-LogAnalyticsQuery -WorkspaceName $WorkspaceName -ResourceGroup $OMSResourceGroupName -SubscriptionId $SubscriptionID -Query $ServicesStoppedQuery -Timespan $isotimespan).Response.Content
-
-
-# Convert results into human readable format
-$ChangeTypesResults = $ChangeTypesResults | ConvertFrom-Json
-$ChangeTypes = $ChangeTypesResults.tables.rows
-
-$TopComputersResults = $TopComputersResults | ConvertFrom-Json
-$TopComputers = $TopComputersResults.tables.rows
-
-$SoftwareAddedResults = $SoftwareAddedResults | ConvertFrom-Json
-$SoftwareAdded = $SoftwareAddedResults.tables.rows
-
-$ServicesStoppedResults = $ServicesStoppedResults | ConvertFrom-Json
-$ServicesStopped = $ServicesStoppedResults.tables.rows
+$ChangeTypesResults = (Invoke-AzureRmOperationalInsightsQuery -WorkspaceId $WorkspaceId -Query $ChangeTypesQuery -Timespan (New-TimeSpan -Days 7)).Results
+$TopComputersResults = (Invoke-AzureRmOperationalInsightsQuery -WorkspaceId $WorkspaceId -Query $TopComputersQuery -Timespan (New-TimeSpan -Days 7)).Results
+$SoftwareAddedResults = (Invoke-AzureRmOperationalInsightsQuery -WorkspaceId $WorkspaceId -Query $SoftwareAddedQuery -Timespan (New-TimeSpan -Days 7)).Results
+$ServicesStoppedResults = (Invoke-AzureRmOperationalInsightsQuery -WorkspaceId $WorkspaceId -Query $ServicesStoppedQuery -Timespan (New-TimeSpan -Days 7)).Results
 
 # Add results to HTML table
 $body = "<h3> Changes per type</h3>"
